7bc9b5d719d5cf33001c50e844d4014d43934a74
thegr8lewis <lewis.nyakaru@gmail.com>
2024-01-30 23:38:12 +0300

    login and sign up details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 //pages and components
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Login from './pages/login'
+import Signup from './pages/signup'
 
 
 function App() {
@@ -12,10 +14,20 @@ function App() {
         <Navbar/>
         <div className="pages">
           <Routes>
-            <Route
-              path='/'
-              element={<Home/>}
-            />
+                <Route
+                  path='/'
+                  element={<Home/>}
+                />
+
+                   <Route 
+                  path="/login" 
+                  element={<Login />} 
+                />
+
+                <Route 
+                  path="/signup" 
+                  element={<Signup />} 
+                />
           </Routes>
         </div>
       </BrowserRouter>
